3ed1c2623537e0ed7cd6d92359d63e40172bfb88
Andrew Deitrick <ajdeitrick@gmail.com>
2015-05-27 11:31:24 -0600

    update user spec

--- spec/features/user_flow_spec.rb
+++ spec/features/user_flow_spec.rb
@@ -5,14 +5,13 @@ feature "as a user" do
 		visit root_path
 		expect(page).to have_link "Sign In"
 		expect(page).to_not have_link "Sign Out"
-		expect(page).to_not have_content "Logged in as Bob Ross"
+		expect(page).to_not have_content "Logged in as bob@example.com"
 		expect(page).to_not have_content "Thanks for signing up!"
 
 		click_on "Sign Up"
 		expect(current_path).to eq(new_user_path)
 
-		fill_in "First Name", with: "Bob"
-		fill_in "Last Name", with: "Ross"
+		fill_in "Email", with: "bob@example.com"
 		fill_in "Password", with: "password"
 		fill_in "Password confirmation", with: "password"
 
@@ -20,35 +19,34 @@ feature "as a user" do
 		expect(current_path).to eq(root_path)
 
 		expect(page).to have_content "Thanks for signing up!"
-		expect(page).to have_content "Logged in as Bob Ross"
+		expect(page).to have_content "Logged in as bob@example.com"
 		expect(page).to_not have_link "Sign In"
 		expect(page).to_not have_link "Sign Up"
 
 		visit current_path
-		expect(page).to have_content "Logged in as Bob Ross"
+		expect(page).to have_content "Logged in as bob@example.com"
 		expect(page).to_not have_content "Thanks for signing up!"
 
 		click_on "Sign Out"
 
 		expect(page).to have_content "Signed out!"
-		expect(page).to_not have_content "Logged in as Bob Ross"
+		expect(page).to_not have_content "Logged in as bob@example.com"
 
 		click_on "Sign In"
 
-		fill_in "First Name", with: "Bob"
-		fill_in "Last Name", with: "Ross"
+		fill_in "Email", with: "bob@example.com"
 		fill_in "Password", with: "password"
 
 		click_button "Sign In"
 		expect(current_path).to eq root_path
 
 		expect(page).to have_content "Thanks for signing in!"
-		expect(page).to have_content "Logged in as Bob Ross"
+		expect(page).to have_content "Logged in as bob@example.com"
 		expect(page).to_not have_link "Sign In"
 		expect(page).to_not have_link "Sign Up"
 
 		visit current_path
-		expect(page).to have_content "Logged in as Bob Ross"
+		expect(page).to have_content "Logged in as bob@example.com"
 		expect(page).to_not have_content "Thanks for signing in!"
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
 	describe "validations" do
 		let(:valid_attributes) {
 			{
-				username: "Sample User", 
+				email: "bobross@example.com", 
 				password: "password", 
 				password_confirmation: "password"
 			}
@@ -15,12 +15,12 @@ describe User do
 			expect(user.valid?).to eq true
 		end
 
-		it "is invalid witout a username" do
-			valid_attributes[:username] = nil
+		it "is invalid witout an email" do
+			valid_attributes[:email] = nil
 			user = User.new(valid_attributes)
 
 			expect(user.valid?).to eq false
-			expect(user.errors[:username]).to eq ["can't be blank"]
+			expect(user.errors[:email]).to eq ["can't be blank"]
 		end
 	end
 end
